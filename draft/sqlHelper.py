93caf8d9bdae50f3592b733554cb504348254ef0
Jamie Chung <jamieschung>
2020-10-01 23:59:39 -0400

    added insertCompany() bc compName is a primary key

--- draft/sqlHelper.py
+++ draft/sqlHelper.py
@@ -3,18 +3,20 @@
 
 import cs304dbi as dbi
 
+uid = "testuser" #for now, until we implement logins
+
 # ==========================================================
 # The functions that do most of the work.
 
 
 # assuming the form has a dropdown of companies and the value stored is the cid.
 # if it's a text field, then change this to inner join company and modify query
-def getByCompany(conn, cid):
+def getByCompany(conn, compName):
     '''Returns the link, cid, uid, position, season, experience, city, state, and country
     of all applications for a specified company, as a list of dictionaries.'''
     curs = dbi.dict_cursor(conn)
     curs.execute('''select * from application
-    where cid = %s;''', [cid])
+    where compName = %s;''', [compName])
     return curs.fetchall()
 
 
@@ -31,16 +33,23 @@ def getByExperience(conn, exp):
     of all applications needing specified experience/year, as a list of dictionaries.'''
     curs = dbi.dict_cursor(conn)
     curs.execute('''select * from application
-    where position FIND_IN_SET ('value', %s);''', [exp])
+    where experience FIND_IN_SET ('value', %s);''', [exp])
     return curs.fetchall()
+
+def insertCompany(compName):
+    conn = dbi.connect()
+    curs = dbi.cursor(conn)
+    curs.execute('''INSERT INTO company(compName) 
+                values (%s);''', [compName])
+    conn.commit()
 
 def insertApplication(link,compName,position,season,yr,experience): #add uid to this once we implement login
     '''Given the link, compName, position, season, yr, experience, inserts an
     application into the database.'''
     conn = dbi.connect()
     curs = dbi.cursor(conn)
-    curs.execute('''INSERT INTO application(link,compName,position,season,yr,experience) 
-                values (%s, %s, %s, %s, %s, %s);''', [link, compName, position, season,
+    curs.execute('''INSERT INTO application(link,compName, uid, position,season,yr,experience) 
+                values (%s, %s, %s, %s, %s, %s, %s);''', [link, compName, uid, position, season, 
                 yr, experience])
     conn.commit()
 
@@ -49,9 +58,12 @@ if __name__ == '__main__':
     dbi.use('techship_db') 
     conn = dbi.connect()
     print("By Company:")
-    print(getByCompany(conn,2))
+    print(getByCompany(conn,"Google"))
     print("By Position:")
     print(getByPosition(conn,"Software Engineering"))
     print("By Experience:")
-    print(getByExperience(conn, "Senior"))
+    # print(getByExperience(conn, "Senior"))
+    # insertApplication("http://www.test.com","test","Data Science","Fall","2022","Freshman")
+    insertCompany("test2")
+    insertApplication("http://www.test2.com","test2","Data Science","Spring","2023","Freshman")
     # use for testing once our tables are populated
