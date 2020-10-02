f7406ab4944dd2174dc9489b130dfa2fb363cfca
Wabil <wasjad@cs.wellesley.edu>
2020-10-01 22:15:41 -0400

    clean up populate_db file

--- draft/backend/sql/populate_db.sql
+++ draft/backend/sql/populate_db.sql
@@ -1,10 +1,4 @@
 
-INSERT INTO company (compName) values ("Chan-Zuckerberg Initiative");
-
-INSERT INTO application (link, position, season, yr, company) values 
-("https://boards.greenhouse.io/embed/job_app?token=2334600", 
-"Software Engineering", "Summer", 2021, 
-(select cid from company where compName ='Chan-Zuckerberg Initiative'));
 
 insert into user(uid,password1,email,school) values ("testuser","tennis0","test@wellesley.edu","Wellesley College");
 
