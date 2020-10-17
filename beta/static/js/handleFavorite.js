08504af6e02ff12252114d3bf164e1c545e80249
Jamie Chung <jamieschung>
2020-10-17 14:29:55 -0400

    cleaned up js file

--- beta/static/js/handleFavorite.js
+++ beta/static/js/handleFavorite.js
@@ -2,20 +2,7 @@
 //handleFavorite.js contains jQuery to handle events when user tries to favorite or unfavorite item
 
 
-////THIS IS ALL USELESS CODE!!!!! but saving it here to help with logic since its similar
-
-//get value of rating clicked and send post request with
-//tt and rating value
-// $('#movies-list').on('click', 'input[name=stars]', function (evt) {
-//     if (!progressive_on) return;
-//     var tt = $(this).closest('tr').attr('data-tt');
-//     var ratingVal = $(this).attr('value');
-//     $(this).css('font-weight', 'bold');
-//     $.post('/rateMovieAjax', { 'tt': tt, 'rating': ratingVal }, updateSingleRating);
-// });
-
 $("input[type=submit]").hide();
-
 
 
 // delegated event handler
@@ -45,7 +32,7 @@ function updateSingleJob(resp) {
 };
 
 
-
+// when loading the page, show which posts are already saved
 function revealButtons(){
     var fave = document.getElementById('favelink').value;
     var internship = document.getElementById('intlink').value;
@@ -57,11 +44,3 @@ function revealButtons(){
         $("#saveicon").css("color","orange");
     }
 }
-//helper function to display new average rating
-// function updateSingleRating(resp) {
-//     var tt = resp['tt'];
-//     var updatedRating = resp['avgRating'];
-//     $('[data-tt=' + tt + ']')
-//         .find('.avgrating').text(updatedRating);
-// };
-
